ed67e25f0f2416137c6952b8d33606b0580fb341
unknown <you@example.com>
2022-07-03 10:51:13 -0700

    Create LogandReg

---
+++ ConsoleApp1/ConsoleApp1/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Login
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        public Login(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+        public bool IsCheckLogEmail(string mail, List<User> users)
+        {
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].Email == mail)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool IsCheckLogPass(string pass, List<User> users)
+        {
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].Password == pass)
+                {
+                    return true;
+
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/UserValidator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class UserValidator : Validator
+    {
+
+        public bool Check(User user)
+        {
+            return IsCheckName(user.Name) & IsCheckLastName(user.SurnName) & IsCheckemail(user.Email);
+
+        }
+        public void Show(User user)
+        {
+            Console.Write(user.Name);
+            Console.WriteLine(user.SurnName);
+        }
+        public bool IsCheckName(string text)
+        {
+            if (IsCheckLentgh(text, 3, 30))
+            {
+                return true;
+            }
+            Console.WriteLine("Name Lentgh is not correct");
+            return false;
+        }
+        public bool IsCheckLastName(string text)
+        {
+            if (IsCheckLentgh(text, 5, 20))
+            {
+                return true;
+            }
+            Console.WriteLine("LastName Lentgh is not correct");
+            return false;
+        }
+
+        public bool IsCheckemail(string mail)
+        {
+            if (IsCheckChar(mail))
+            {
+                return true;
+            }
+            Console.WriteLine("Mail is Not Correct");
+            return false;
+        }
+        public bool IsCheckUnikalEmail(string text)
+        {
+            if (Program.IsCheckUnikal(text))
+            {
+                return true;
+            }
+            Console.WriteLine("Email exists ");
+            return false;
+        }
+
+        internal void Show(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
